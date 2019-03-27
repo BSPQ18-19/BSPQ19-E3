6b4da6dbece1a94526db423aef32a680bf64f64c
albertofdr <albertofdr@gmail.com>
2019-03-27 17:11:18 +0100

    Initial configurations

---
+++ src/main/java/es/deusto/server/Server.java
@@ -0,0 +1,65 @@
+package es.deusto.server;
+
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
+import javax.jdo.Query;
+import javax.jdo.JDOHelper;
+import javax.jdo.Transaction;
+
+import es.deusto.server.jdo.User;
+import es.deusto.server.*;
+
+public class Server extends UnicastRemoteObject implements IServer {
+
+	private static final long serialVersionUID = 1L;
+	private int cont = 0;
+	private PersistenceManager pm=null;
+	private Transaction tx=null;
+
+	protected Server() throws RemoteException {
+		super();
+		PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory("datanucleus.properties");
+		this.pm = pmf.getPersistenceManager();
+		this.tx = pm.currentTransaction();
+	}
+	
+	protected void finalize () throws Throwable {
+		if (tx.isActive()) {
+            tx.rollback();
+        }
+        pm.close();
+	}
+
+	
+	public void registerUser(String login, String password) {
+		//Register User
+		
+	}
+
+	public static void main(String[] args) {
+		if (args.length != 3) {
+			System.out.println("How to invoke: java [policy] [codebase] Server.Server [host] [port] [server]");
+			System.exit(0);
+		}
+
+		if (System.getSecurityManager() == null) {
+			System.setSecurityManager(new SecurityManager());
+		}
+
+		String name = "//" + args[0] + ":" + args[1] + "/" + args[2];
+
+		try {
+			IServer objServer = new Server();
+			Naming.rebind(name, objServer);
+			System.out.println("Server '" + name + "' active and waiting...");
+			
+		} catch (Exception e) {
+			System.err.println("Hello exception: " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/main/java/es/deusto/server/jdo/User.java
@@ -0,0 +1,41 @@
+package es.deusto.server.jdo;
+
+import java.util.Set;
+
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.PrimaryKey;
+import javax.jdo.annotations.Join;
+import javax.jdo.annotations.Persistent;
+import java.util.HashSet;
+
+@PersistenceCapable
+public class User {
+	@PrimaryKey
+	String login=null;
+	String password=null;
+	
+	
+	
+	
+	public User(String login, String password) {
+		this.login = login;
+		this.password = password;
+	}
+	
+
+	public String getLogin() {
+		return this.login;
+	}
+	
+	public String getPassword() {
+		return this.password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	 
+}
+
